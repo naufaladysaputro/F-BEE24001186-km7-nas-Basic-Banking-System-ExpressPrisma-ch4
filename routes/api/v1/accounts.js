2a990097bda4579226c77d762abaa1eed31c54b6
naufaladysaputro <157557765+naufaladysaputro@users.noreply.github.com>
2024-10-18 20:40:19 +0700

    add: controller,router accounts

---
+++ routes/api/v1/accounts.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const AccountsController = require("../../../controllers/accounts.controller");
+const router = express.Router();
+
+router.get("/", AccountsController.getAllAccounts);
+router.get("/:id", AccountsController.getAccountById);
+router.post("/", AccountsController.createAccount);
+router.put("/:id", AccountsController.updateAccount);
+router.delete("/:id", AccountsController.deleteAccount);
+
+module.exports = router;
--- routes/api/v1/index.js
+++ routes/api/v1/index.js
@@ -1,7 +1,9 @@
 var express = require("express");
 var router = express.Router();
 const USER_ROUTER = require("./users");
+const ACCOUNT_ROUTER = require("./accounts");
 
 router.use("/users", USER_ROUTER);
+router.use("/accounts", ACCOUNT_ROUTER);
 
 module.exports = router;
